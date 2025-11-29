029b3eca742bb6ba8a290f07a83007a70a192b7a
enzo <e.ferrari449@gmail.com>
2025-11-29 12:21:11 -0300

    practica tests chequeo suma

--- tests_chequeo.py
+++ tests_chequeo.py
@@ -4,16 +4,6 @@
  * su resultado.
  * Crea un test, utilizando las herramientas de tu lenguaje, que sea
  * capaz de determinar si esa función se ejecuta correctamente.
- *
- * DIFICULTAD EXTRA (opcional):
- * Crea un diccionario con las siguientes claves y valores:
- * "name": "Tu nombre"
- * "age": "Tu edad"
- * "birth_date": "Tu fecha de nacimiento"
- * "programming_languages": ["Listado de lenguajes de programación"]
- * Crea dos test:
- * - Un primero que determine que existen todos los campos.
- * - Un segundo que determine que los datos introducidos son correctos.
  """
 
 num_1 = int(input("ingrese el primer numemro a sumar: "))
@@ -41,3 +31,38 @@ class testSuma(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+"""
+* DIFICULTAD EXTRA (opcional):
+ * Crea un diccionario con las siguientes claves y valores:
+ * "name": "Tu nombre"
+ * "age": "Tu edad"
+ * "birth_date": "Tu fecha de nacimiento"
+ * "programming_languages": ["Listado de lenguajes de programación"]
+ * Crea dos test:
+ * - Un primero que determine que existen todos los campos.
+ * - Un segundo que determine que los datos introducidos son correctos.
+"""
+
+
+class TestData(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = { 
+        "name": "Enzo",
+        "age": "21",
+        "birth_date": "09-08-2004",
+        "programming_languajes": "[python, typescript, react]"
+        }
+
+    def text_fields_exists(self):
+        self.assertIn("name", self.data)
+        self.assertIn("age", self.data)
+        self.assertIn("birth_date", self.data)
+        self.assertIn("programming_languages", self.data)
+
+    def test_data_is_correct(self):
+        self.assertIsInstance(self.data["name", str])
+        self.assertIsInstance(self.data["age"], int)
+        self.assertIsInstance(self.data["birth_date"], date)
+        self.assertIsInstance(self.data["programming_languages"], list)
+unittest.main()
